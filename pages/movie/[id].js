654688e5969cc4e263e1b77ecc7080e43146581f
ndjordjevic990 <n.djordjevic990@gmail.com>
2021-11-20 19:37:21 +0100

    styling the movie page WIP

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,5 +1,103 @@
-function Movie() {
-  return <div>Movie</div>;
+import Head from "next/head";
+import Image from "next/image";
+import Header from "../../components/Header";
+import { useSession, getSession } from "next-auth/client";
+import { LogInPage } from "../../components/LogInPage";
+import { Result } from "postcss";
+import { PlusIcon } from "@heroicons/react/solid";
+import { useState } from "react";
+
+function Movie({ movie }) {
+  const [session] = useSession();
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  const [setPlayer, setShowPlayer] = useState(false);
+
+  return (
+    <div>
+      <Head>
+        <title>{movie.title || movie.original_name}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+      {!session ? (
+        <LogInPage />
+      ) : (
+        <section className="relative z-50">
+          <div className="relative min-h-[calc(100vh-72px)]">
+            <Image
+              src={
+                `${BASE_URL}${movie.backdrop_path || movie.poster_path}` ||
+                `${BASE_URL}${movie.poster_path}`
+              }
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+          <div className="absolute inset-y-28 md:inset-y-auto md:bottom-10 inset-x-4 md:inset-x-12 space-y-6 z-50">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
+              {movie.title || movie.original_name}
+            </h1>
+            <div className="flex items-center space-x-3 md:space-x-5">
+              <button className="text-sm md:text-base bg-[#f9f9f9] text-black flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6] ">
+                <img
+                  src="/images/play-icon-black.svg"
+                  alt=""
+                  className="h-6 md:h-8"
+                />
+                <span className="uppercase font-medium tracking-wide">
+                  Play
+                </span>
+              </button>
+              <button
+                className="text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] flex items-center justify-center py-2.5 px-6 rounded hover:bg-[#c6c6c6]"
+                onClick={() => setShowPlayer(true)}
+              >
+                <img
+                  src="/images/play-icon-white.svg"
+                  alt=""
+                  className="h-6 md:h-8"
+                />
+                <span className="uppercase font-medium tracking-wide">
+                  Trailer
+                </span>
+              </button>
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                <PlusIcon className="h-6" />
+              </div>
+
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                <img src="/images/group-icon.svg" alt="" />
+              </div>
+            </div>
+            <p className="text-xs md:text-sm">
+              {movie.release_date || movie.first_air_date} •{" "}
+              {Math.floor(movie.runtime / 60)}h {movie.runtime % 60}m •{" "}
+              {movie.genres.map((genre) => genre.name + " ")}{" "}
+            </p>
+            <h4 className="text-sm md:text-lg max-w-4xl">{movie.overview}</h4>
+          </div>
+        </section>
+      )}
+    </div>
+  );
 }
 
 export default Movie;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  // extract from url
+  const id = context.query.id;
+
+  const movie = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      session,
+      movie,
+    },
+  };
+}
